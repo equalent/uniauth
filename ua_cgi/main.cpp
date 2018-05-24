28a5fd5b3d00bd07df8f90717434a4129afc3891
Andrey Tsurkan <andrey.turcan@hotmail.com>
2018-05-24 19:38:47 +0300

    Installed Boost libraries

--- ua_cgi/main.cpp
+++ ua_cgi/main.cpp
@@ -1,9 +1,18 @@
+/*
+############################# THE UNIAUTH PROJECT #############################
+(c) 2018, Andrey Tsurkan
+This project is released under GNU GPL license
+###############################################################################
+*/
 #include <iostream>
+#include <cstdlib>
+#include "json.hpp"
+#include "filesystem.hpp"
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello world!" << endl;
+    cout<<argv[0];
     return 0;
 }
---
+++ ua_cgi/setup.cpp
@@ -0,0 +1,7 @@
+/*
+############################# THE UNIAUTH PROJECT #############################
+(c) 2018, Andrey Tsurkan
+This project is released under GNU GPL license
+###############################################################################
+*/
+//The installation "wizard" file
